1ad4f1670482e7897e77a7d864204c80a812b60e
Pavel Semenov <pavel@Pavels-MacBook-Pro.local>
2024-12-12 20:31:32 +0100

    Webpage updates

--- web/src/pages/downloads.js
+++ web/src/pages/downloads.js
@@ -11,23 +11,19 @@ const downloadStyles = {
 const Downloads = () => {
     const thesisURL = process.env.DOWNLOAD_THESIS_URL;
     const demoURL = process.env.DOWNLOAD_DEMO_URL;
-    const executableURL = process.env.DOWNLOAD_EXECUTABLE_URL;
-    const sourceCodeURL = process.env.DOWNLOAD_SOURCE_CODE_URL;
+    const seminarPresentationURL = process.env.SEMINAR_PRESENTATION_URL;
     return (
         <div style={downloadStyles} className={'downloads'}>
             <h2 className="text-center">Downloads</h2>
             <ul>
                 <li>
-                    <a href="">Thesis</a>
+                    <a href={{thesisURL}}>Thesis</a>
                 </li>
                 <li>
-                    <a href="">Demo</a>
+                    <a href={{demoURL}}>Demo</a>
                 </li>
                 <li>
-                    <a href="">Executable</a>
-                </li>
-                <li>
-                    <a href="">Source code</a>
+                    <a href={{executableURL}}>Project Seminar 2 presentation</a>
                 </li>
             </ul>
         </div>
